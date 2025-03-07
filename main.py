683f13c5e9c4a37e38939282578c8b72523a6a6a
ProgamerPY <victorritossantos@gmail.com>
2025-03-07 19:43:20 -0300

    update

---
+++ main.py
@@ -0,0 +1,13 @@
+from flask import Flask
+from flask import render_template
+
+
+web = Flask(__name__)
+
+@web.route("/") #HOME
+def homepage():
+    return render_template("homepage.html")
+
+
+if __name__ == "__main__":
+    web.run()
